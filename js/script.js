187a498d8404f49de9cea98e3838a83e20cae6f5
alexisbec <alexisbec13@hotmail.com>
2022-08-31 13:20:29 -0500

    Add navbar demo

--- js/script.js
+++ js/script.js
@@ -11,3 +11,32 @@ $(function () {
             }
         });
 });
+
+$(document).ready(function() {
+    function toggleSidebar() {
+        $(".button").toggleClass("active");
+        $("main").toggleClass("move-to-left");
+        $(".sidebar-item").toggleClass("active");
+    }
+
+    $(".button").on("click", function() {
+        toggleSidebar();
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode === 27) {
+            toggleSidebar();
+        }
+    });
+});
+
+$(function(){
+    var value2upd = function(){
+        var w = $(window).width();
+        return w < 767 ? "zoom-in" : "zoom-out";
+    };
+
+    $(window).resize(function(){
+        $('.subtitle-quienes-somos').attr('data-aos', value2upd);
+    }).resize();
+});
